98ed5a185cb43d00192538a7a63d29e68196be46
eyueksel06 <eyueksel@student.tgm.ac.at>
2025-12-12 12:24:22 +0100

    environment.ts verbessert

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,25 @@
 import { ApplicationConfig } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging()),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-81","appId":"1:401541735108:web:4451aea83c8a0668125f91","storageBucket":"friendlychat-81.firebasestorage.app","apiKey":"AIzaSyDEOElhj4BjVLV1KDw-gkgOIi-CFs8c_sg","authDomain":"friendlychat-81.firebaseapp.com","messagingSenderId":"401541735108","projectNumber":"401541735108","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideRouter(routes),
+    // 1. Initialisierung über die environment-Datei (Empfohlen)
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    
+    // Auth, Firestore, etc.
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging()),
   ],
 };
